56a41a91adebaab72685d857529b19f3bb69c4bd
justina823 <62530557+justina823@users.noreply.github.com>
2021-01-19 13:43:24 +1000

    movie review plug in

---
+++ wp-content/plugins/reviews/movie-reviews.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Reviews
+Description: Review post type with all the trimmings
+Author: Justina Chan
+Version: 1.0
+
+*/
+
+class My_Movie_Reviews {
+  private static $instance;
+
+  public static function getInstance() {
+    if (self::$instance == NULL) {
+      self::$instance = new self();
+    }
+
+    return self::$instance;
+  }
+
+  private function __construct() {
+    // implement hooks here
+    add_action('init', array($this,'register_post_type'));
+  }
+  function register_post_type(){
+    register_post_type('movie_review',array(
+      'labels'=> array(
+        'name' =>  ('Movie Reviews'),
+        'singular_name' =>  ('Movie Review'),
+      ),
+      'description' =>  ('Highly opinionated movie review'),
+      'supports' =>  array(
+        'title','editor','excerpt','author','revisions','thumbnail','custom-fields',
+      ),
+      'public' => TRUE, 
+
+    ));
+  }
+}
+
+My_Movie_Reviews::getInstance();
